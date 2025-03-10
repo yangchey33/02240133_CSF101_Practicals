4727578512a452604dd7b969106d07ca0461aafb
yangchey33 <02240133.cst@rub.edu.bt>
2025-03-09 23:34:02 -0800

    My second commit

---
+++ practical1.py
@@ -0,0 +1,17 @@
+name =("Yangchen")
+print(name)
+small = True
+print(small)
+
+a = 5
+b = 7
+c = a*b
+print(c)
+
+Dorji = 23
+Tobgay = 20
+if Dorji>Tobgay:
+
+    print("Dorji is older than Tobgay")
+else:
+    print("Kezang is older than Dorji")
